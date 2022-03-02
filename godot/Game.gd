bec85fe157a76194e61212086580c59e8c824e0f
Aezart Rothenus <heroofhyla@gmail.com>
2022-03-02 15:03:04 -0700

    Add last click tracker

--- godot/Game.gd
+++ godot/Game.gd
@@ -1,8 +1,17 @@
 extends Node2D
 
+var last_click_cell = Vector2.ZERO
 
 func get_mouse_pos() -> Vector2:
 	return get_global_mouse_position()
 
 func get_mouse_cell() -> Vector2:
 	return $TileMap.world_to_map(get_mouse_pos())
+
+func _unhandled_input(event) -> void:
+	if event is InputEventMouseButton:
+		handle_mouse_click(event)
+
+func handle_mouse_click(event: InputEventMouseButton) -> void:
+	if event.button_index == BUTTON_LEFT and event.pressed:
+		last_click_cell = get_mouse_cell()
--- godot/MouseTracker.gd
+++ godot/MouseTracker.gd
@@ -2,4 +2,5 @@ extends Label
 
 onready var game = get_node("/root/Game")
 func _process(_delta):
-	text = str(game.get_mouse_pos()) + "\n" + str(game.get_mouse_cell())
+	text = str(game.get_mouse_pos()) + "\n" + str(game.get_mouse_cell()) + "\n" + "Last clicked cell: " + str(game.last_click_cell)
+	
